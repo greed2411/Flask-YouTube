d34d4c53346ede3054396c4eac920c187e152931
Greed <jaiimmortal@gmail.com>
2017-06-25 21:23:05 +0530

    Integrated flask and ydl
    Able to download videos from YouTube to the server

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, flash, url_for
-
+from ydl import get_media
 
 app = Flask(__name__)
 
@@ -11,22 +11,26 @@ def home_page():
 	if request.method == 'POST':
 		
 			attempted_url = request.form['url']
-			FACILITIES ={"Audio": "1",
-					 "Video": "2",
-					 "Audio Playlist": "3",
-					 "Video Playlist": "4"
+			CHOICES ={"Audio": 1,
+					 "Video": 2,
+					 "Audio Playlist": 3,
+					 "Video Playlist": 4
 					}
-			attempted_choice = list(FACILITIES.keys())[list(FACILITIES.values()).index(str(request.form['submit']))]
-			user_options = [attempted_url, attempted_choice]
-			return redirect(url_for('home_page'))
-			#return render_template('index.html', title = user_options)
-		
+			attempted_choice = list(CHOICES.keys())[list(CHOICES.values()).index(int(request.form['submit']))]
+			num_choice = CHOICES.get(attempted_choice)
+			user_options = [attempted_url, num_choice]
+			result = get_media(attempted_url, num_choice)
+					
 	return render_template('index.html', name_of_company = name_of_company, download_location = download_location, title = title)
 	
 
 @app.errorhandler(404)
 def page_not_found(error):
-    return render_template('page_not_found.html'), 404
+    return render_template('page_not_found.html' , "404 bud"), 404
+	
+@app.errorhandler(400) 
+def page_not_found(error): 
+	return render_template('invalid.html', title = "Invalid URL"), 400
 
 if __name__ == '__main__':
 	
