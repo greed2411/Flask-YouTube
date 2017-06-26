a98a03d9dfa3ec5993be521d735bc83c528f7a73
Greed <jaiimmortal@gmail.com>
2017-06-26 10:51:10 +0530

    Faster version
    Removed unnecessary functions from the perspective of server and generalized the errors into categories:
    bad request, 404, invalid url

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, flash, url_for
-from ydl import get_media
+from ydl import get_media, verify
 
 app = Flask(__name__)
 
@@ -8,29 +8,47 @@ def home_page():
 	title = "YDL"
 	name_of_company = "YouTube Downloader"
 	download_location = "YouTube URL"
+	
 	if request.method == 'POST':
 		
 			attempted_url = request.form['url']
+			
 			CHOICES ={"Audio": 1,
 					 "Video": 2,
 					 "Audio Playlist": 3,
 					 "Video Playlist": 4
 					}
+			
 			attempted_choice = list(CHOICES.keys())[list(CHOICES.values()).index(int(request.form['submit']))]
 			num_choice = CHOICES.get(attempted_choice)
-			user_options = [attempted_url, num_choice]
-			result = get_media(attempted_url, num_choice)
-					
+
+			if verify(attempted_url) :
+
+				result = get_media(attempted_url, num_choice)
+				return redirect(url_for('home_page'))
+			
+			return render_template('error_template.html' , title = "Invalid URL", 
+															message = "Invalid URL",
+    													   subline = "Welp :|", 
+    													   image_location = url_for('static', filename = 'images/house-simpson.jpg'))
+
+
 	return render_template('index.html', name_of_company = name_of_company, download_location = download_location, title = title)
 	
 
 @app.errorhandler(404)
 def page_not_found(error):
-    return render_template('page_not_found.html' , title = "404 bud"), 404
+    return render_template('error_template.html' , title = "404 bud", 
+    												message = "Page not found or doesn't exist",
+    												subline = "Welp :|", 
+    												image_location = url_for('static', filename = 'images/queen-band-simpson.jpg') ), 404
 	
 @app.errorhandler(400) 
-def page_not_found(error): 
-	return render_template('invalid.html', title = "Invalid URL"), 400
+def bad_request(error): 
+	return render_template('error_template.html' , title = "Aaaah ...", 
+													message = "We don't know what happened",
+    												subline = "Probably because you didn't give a choice of download and/or a valid URL", 
+    												image_location = url_for('static', filename = 'images/gangam-style-simpson.jpg')), 400
 
 if __name__ == '__main__':
 	
--- ydl.py
+++ ydl.py
@@ -1,10 +1,4 @@
 import subprocess
-import os
-import urllib.request
-import urllib.error
-import sys
-import bs4
-
 
 """
 	YouTube Downloader used to download the best quality 
@@ -17,106 +11,39 @@ import bs4
 	Started working on : 21-06-2017
 	
 """
-
-def internet_on():
-	"""
-		Checking whether connected to the internet 
-		if not connected to internet but connected to wifi, not yet logged in, 
-		returns a '200' code for trying to fetch results (wifi defualt login page)
-		URLError if there is no internet or default wifi redirecting, AttributeError for handling 'Nonetype' object (response) in the above
-	"""
-	try:
-		
-		response = urllib.request.urlopen('http://google.com', timeout=1)
-		soup = bs4.BeautifulSoup(response, "html.parser") 						 
-		if soup.title.text == 'Google': 								  
-		
-			return True
-
-	except (urllib.request.URLError, AttributeError) : 
-		
-		return False 	
-
 def verify(url):
-	"""
-		Verifying URL, whether it's a URL, whether that URL belongs to YouTube or not
-	"""
-	
-	checking_internet = internet_on()
-	
-	if checking_internet == True:
-		
-		try :
-			
-			separation = url.split('/')  
-			
-			if separation[2] == 'www.youtube.com' or separation[2] == 'youtu.be':
-				
-				"""
-					Not checking whether URL is a playlist URL or not because of 
-					YouTube's new User's 'My Mix' of songs, which don't have a 'playlist' in their URL
-				"""
-				
-				#print('URL belongs to YouTube')		
-				return True
-			
-			else:
-				
-				return "Not a YouTube URL" # test case : 'https://mail.google.com/mail/u/0/#inbox'
-		
-		except Exception:
-			
-			return 'Oops , Not a valid URL' # test case : 'affdfsffasf'
-	
-	else:
-		
-		return "Hmmm... you're not connected to the Internet"
 
+	if 'www.youtube.com' in url or 'youtu.be' in url :
+		return True
+	return False
 
 def get_media(url, choice):
 	
 	try:
-		
-		check = verify(url)
-		if check == True:
-			
-			if choice == 1:
 				
-				subprocess.call('youtube-dl -f 251 -o "/media/Audio downloads from youtube-dl/%(title)s.%(ext)s" -q --no-playlist --extract-audio --audio-format mp3 --no-warnings "{url}"'.format(url=url), shell=True)
-				#print('\n\nThe process is over and your file is probably residing in ' + os.getcwd() + '\\media\Audio downloads from youtube-dl' )
-			
-			elif choice == 2:
+		if choice == 1:
 				
-				subprocess.call('youtube-dl  -o "/media/Video downloads from youtube-dl/%(title)s.%(ext)s" -q --no-playlist --no-warnings "{url}"'.format(url=url), shell=True)
-				#print('\n\nThe process is over and your file is probably residing in ' + os.getcwd() + '\\media\Video downloads from youtube-dl' )
-			
-			elif choice == 3:
+			subprocess.call('youtube-dl -f 251 -o "/media/Audio downloads/%(title)s.%(ext)s" -q --no-playlist --extract-audio --audio-format mp3 --no-warnings "{url}"'.format(url=url), shell=True)
+						
+		elif choice == 2:
 				
-				subprocess.call('youtube-dl -i -o "%/media/(playlist)s/%(playlist_index)s.%(title)s.%(ext)s" --yes-playlist --extract-audio --audio-format mp3 --no-warnings "{url}"'.format(url=url), shell=True)
-				#print('\n\nThe process is over and currently residing in the current working directgory with the name of the folder same as that of playlist!')
-			
-			elif choice == 4:
+			subprocess.call('youtube-dl  -o "/media/Video downloads/%(title)s.%(ext)s" -q --no-playlist --no-warnings "{url}"'.format(url=url), shell=True)
+						
+		elif choice == 3:
 				
-				subprocess.call('youtube-dl -i -o "%/media/(playlist)s/%(playlist_index)s.%(title)s.%(ext)s" --yes-playlist --newline --no-warnings "{url}"'.format(url=url), shell=True)
-				#print('\n\nThe process is over and currently residing in the current working directgory with the name of the folder same as that of playlist!')
-			return "Success!!"
-		
-		elif check == "Not a YouTube URL" :
-			
-			return "Not a YouTube URL"
-		
-		elif check == 'Oops , Not a valid URL' :
-		
-			return 'Oops , Not a valid URL'
-		
+			subprocess.call('youtube-dl -i -o "%/media/Audio Playlists/(playlist)s/%(playlist_index)s.%(title)s.%(ext)s" --yes-playlist --extract-audio --audio-format mp3 --no-warnings "{url}"'.format(url=url), shell=True)
+						
+		elif choice == 4:
 				
-			
+			subprocess.call('youtube-dl -i -o "%/media/Video Playlists/(playlist)s/%(playlist_index)s.%(title)s.%(ext)s" --yes-playlist --newline --no-warnings "{url}"'.format(url=url), shell=True)		
+					
 	except Exception as e: #for any other unknown errors,  used it to get the other exceptions mentioned above
 		
 		return e
 
 url = ""
-choice = 1		
+choice = 1
+verify(url)		
 get_media(url, choice)
 	
 
